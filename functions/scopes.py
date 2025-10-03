3482db88ce2dfdde771b93235fb373cc1f77294e
akashvharale <akash.v.harale@gmail.com>
2025-10-03 23:14:48 +0530

    scopes in functions

---
+++ functions/scopes.py
@@ -0,0 +1,11 @@
+def chaiCounter():
+    chai_order= "Lemon chai"
+    
+    def print_order():
+        chai_order= "Black chai"
+        print(chai_order)
+        
+    print_order()
+    print(chai_order)
+    
+chaiCounter()
