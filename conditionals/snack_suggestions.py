39e8e5c6be377e726571f70e8270d0359a2236b1
akashvharale <akash.v.harale@gmail.com>
2025-10-03 15:22:41 +0530

    if else practice

---
+++ conditionals/snack_suggestions.py
@@ -0,0 +1,6 @@
+snack= input("What snack do you wanna eat today: ").lower()
+
+if snack=="samosa" or snack =="cookie":
+    print(f"Nice Choice !, I will serve your snacks in a minute")
+else:
+    print(f"Sorry we serve only samosa and cookie today")
