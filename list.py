98119d5a401bd4165a3a49baf138ce00dd54aaef
akashvharale <akash.v.harale@gmail.com>
2025-10-03 12:20:23 +0530

    list in python

---
+++ list.py
@@ -0,0 +1,19 @@
+listOfBooks=["Vande Bharat", "Aajivan karavas", "Wings of Fire"];
+print(f"{listOfBooks}")
+
+listOfBooks.append("My Book")
+print(f"{listOfBooks}")
+
+listOfBooks.pop()
+print(f"{listOfBooks}")
+
+listOfBooks.remove("Vande Bharat")
+print(f"{listOfBooks}")
+
+listOfBooks.sort()
+print(f"{listOfBooks}")
+
+listOfBooks.reverse()
+print(f"{listOfBooks}")
+
+
