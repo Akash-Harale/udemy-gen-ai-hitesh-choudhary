1dbb2277d2bbf89ff421ba5f163cab179c43195a
akashvharale <akash.v.harale@gmail.com>
2025-10-03 22:53:33 +0530

    for loop

---
+++ loops/forLoop.py
@@ -0,0 +1,30 @@
+for el in range(10):
+    print(el)
+    
+print(f"new line")
+    
+for el in range(1,11):
+    print(el)
+    
+print(f"new line")
+
+for el in range(1,11,2): # step size
+    print(el)
+    
+print(f"new line")
+
+std_name=["Ram", "Shyam", "Hari", "Gita", "Sita"]
+
+for name in std_name:
+    print(f"Hello {name}")
+
+print(f"new line")
+
+# for loop with enumerate
+
+for el in enumerate(std_name): # 
+    print(el)
+    
+print(f"new line")
+
+
